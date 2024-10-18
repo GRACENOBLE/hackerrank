7bc4907b0f1692bb161de9a733435bb30cb921fd
GRACENOBLE <gracenoble72@gmail.com>
2024-10-18 11:15:37 +0300

    separated functions

---
+++ functions.go
@@ -0,0 +1,5 @@
+package main
+
+func printSomethng(){
+	print("hello world")
+}
--- hackerrank.go
+++ hackerrank.go
@@ -1,37 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
-
 func main(){
-	s := "12:45:54PM"
-	time := s[:len(s)-2]
-	symbol := s[len(s)-2:]
-	
-	if symbol == "AM"{
-		hour := time[:2]
-		
-		if hour == "12" {
-			hour = "00"
-		}
-		
-		print(hour+time[2:])
-		return
-	}
-
-	hour, err := strconv.Atoi(s[:2])
-
-	if err != nil {
-		fmt.Println("Error:", err)
-	} else {
-		
-		if hour != 12{
-			hour += 12
-		}
-	}
-
-	print(strconv.Itoa(hour) + s[2:len(s)-2])
-
+	printSomethng()
 }
